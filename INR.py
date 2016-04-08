904b9623659b20e0ff770d7de99cc97f1e2b4e8f
yuriprym <samuel.young.103@gmail.com>
2016-04-08 13:49:47 -0400

    add the sum of all the individual mathatmatic steps and creaed the individual parts to try and impliment

---
+++ INR.py
@@ -0,0 +1,36 @@
+!/bin/bash/python
+''''
+is to try and create a python like tester file to  see if we can obtain the international normalized ratio and the ptt of blood.
+''''
+#### variables ##### 
+inr = 0.0
+pt = 0.0
+ptnom = 0.0
+isitest=0
+slope=0
+isiref=0
+user_data_low = 0.0
+user_data_high = 0.0
+tester_data =0.0
+a= 
+#### main program #####
+user_data_low = input{'prompt the users to enter a two digit number '}
+user_data_high = input{'prompt the users to enter a two digit number '}
+
+### i got to figure out how to make INR
+
+
+
+
+
+#### isi 
+
+
+
+
+#### writing it to a file then adding it to that and printing it out
+
+
+### slope
+
+slope = (a-c)/(b-c)
