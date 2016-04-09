0ebb47ff7094eb503a296f06c1c14d044882e2ad
yuriprym <samuel.young.103@gmail.com>
2016-04-09 17:20:44 -0400

    add features to the GPIOs that I used

--- iNR.py
+++ iNR.py
@@ -3,7 +3,12 @@
 ###is to try and create a python like tester file to  see if we can obtain the international normalized ratio and the ptt of blood.
 
 #### variables ##### 
-
+import time
+import numpy as np
+import sys
+import RPi.GPIO as GPIO
+import threading 
+import collections
 
 f=open(~/Documents/bloodtest,'r+a')
 inr = 0.0
@@ -19,8 +24,27 @@ a=0
 b=0
 c=0
 i=0
-#### main program #####
+##### GPIO  #######
+GPIO,setmode(GPIO.BOARD)
+GPIO.cleanup()
+readout1=38
+readout2=37
+readout3=36
+heating1=01
+heating2=02
+heating3=04
+ground1=06
+ground2=25
+ground3=14
+### setting up the pins
+GPIO.setup(readout1, GPIO.IN)
+GPIO.setup(heating1, GPIO.OUT)
+GPIO.setup(readout2, GPIO.IN)
+GPIO.setup(heating2, GPIO.OUT)
+GPIO.setup(readout3, GPIO.IN)
+GPIO.setup(heating3, GPIO.OUT)
 
+#### main program #####
 while(i== 1 and n==exit):
 
    user_data_low = input('prompt the users to enter a two digit number ')
@@ -33,11 +57,11 @@ while(i== 1 and n==exit):
       if (i !=1 and n==reset):
          i=i+1
          if(i == 1 or n==reset):
-            print(inr+' and '+  pt)
+            print(int_nom+ratio() +"and"+ pt_for_inr())
             if(n==input('just type reset')):
                 n = input('type exit if you have or accidental wanted to restart to quit other wise it not going to prefrom a last check')
    if (n!=1 or n < 1 and  n==exit):
-      print(inr+" and "+  pt)
+      print(int_nom_rat()+  " and "+  pt_())
    
 ### i got to figure out how to make INR calculation
 class int_nom_rat:
@@ -72,6 +96,16 @@ class final:
         return
      else:
         f.readline(size-1)
-
+		f.close()
+class pt__for_inr:
+   def pt_(self):
+	  f1 = open(datapoints)
+	  pt1 = input(pt1)
+	  pt2 = input(pt2)
+	  pt3 = input(pt3)
+	  pt4 = input(pt4)
+	  pt5 = input(pt5)
+	  pt6 = input(pt6)
+	  pt_self=alanysis() 
 
 #### writing it to a file then adding it to that and printing it out
