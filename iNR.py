3e84d454dc8ba69a0b7246de878af2be3069a19a
yuriprym <samuel.young.103@gmail.com>
2016-04-08 22:15:17 -0400

    printing and writing data on a file

--- iNR.py
+++ iNR.py
@@ -3,6 +3,7 @@
 is to try and create a python like tester file to  see if we can obtain the international normalized ratio and the ptt of blood.
 ''''
 #### variables ##### 
+f=open(bloodtest.ace2,'r+a')
 inr = 0.0
 pt = 0.0
 ptnom = 0.0
@@ -16,13 +17,23 @@ a=0
 b=0
 c=0
 #### main program #####
-user_data_low = input{'prompt the users to enter a two digit number '}
-user_data_high = input{'prompt the users to enter a two digit number '}
 
+while(i= 1 and n=resest):
+	  i=0
+   user_data_low = input{'prompt the users to enter a two digit number '}
+   user_data_high = input{'prompt the users to enter a two digit number '}
+   f.write(user_data_low)
+   f.write(user_data_high)
+   while (i <100000 or n= exit):
+   int_nor-rat():
+   print(int_nor_rat()
+   f.write(inr pt)
+   i=i+1
+   
 ### i got to figure out how to make INR calculation
 
-inr = (pt/ptnom)^isitest
-
+int_nor_rat(inr):
+   inr = (pt/ptnom)^isitest
 
 
 #### isi 
