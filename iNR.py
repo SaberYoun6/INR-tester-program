7a7818647dc74b65339a21984c893d3904084b45
yuriprym <samuel.young.103@gmail.com>
2019-08-23 14:57:16 -0500

    changed the timer to see if it could detect the light

--- iNR.py
+++ iNR.py
@@ -12,7 +12,7 @@ __author__ = "Samuel Young"
 __copyright__ =" Copyright 2016, The INR project"
 __credits__ = ["Samuel Young"]
 __license__ = "GPL"
-__versoion__ = "0.0.5"
+__version__ = "0.0.5"
 __maintianer__= "Samuel Young"
 __email__ = "samuel.young.103@gmail.com"
 __status__ = "pre-beta"
@@ -46,11 +46,11 @@ class LightSenor(object):
         GPIO.setup(self.reflection, GPIO.OUT)
         GPIO.output(self.reflection,GPIO.LOW)
         GPIO.setup(self.light,GPIO.OUT) 
-        led_off= GPIO.output(self.light,GPIO.LOW)
-        time.sleep(7.5)
+        #led_off= GPIO.output(self.light,GPIO.LOW)
+        #time.sleep(7.5)
         print("testing light")
         led_on=GPIO.output(self.light,GPIO.HIGH)
-        time.sleep(7.5)
+        time.sleep(1)
         print("Setup the input pin")
         GPIO.setup(self.reflection,GPIO.IN)
         print( "Firing up the collection tools")
@@ -60,10 +60,11 @@ class LightSenor(object):
             print("pin input authorized")
             refc= GPIO.input(self.reflection)
             led_on
+            time.sleep(.0000001)
             final_time = time.perf_counter()
-            timer2=time.time()
+            timer2 = time.time()
             count = count + 1
-            print (count)
+            print(count)
             print(value)
             print(refc)
             if refc == 0:
@@ -76,21 +77,22 @@ class LightSenor(object):
                    print("exceed time granted")
                    delta_time = final_time - init_time
                    print("Turning off LED")
-                   led_off
+                   GPIO.output(self.light,GPIO.LOW)
                    break
             elif .01<= refc and refc <= .99:
                print("LED on")
                led_on
+               time.sleep(.000001)
                if count == 359901:
                    value = True
                    print("exceed time granted")
                    delta_time = final_time - init_time
                    print("Turning off LED")
-                   led_off
+                   GPIO.output(self.light,GPIO.LOW)
                    break
             else: 
                print("LED OFF")
-               led_off
+               GPIO.output(light,GPIO.LOW)
                value = True
                delta_time = final_time - init_time
         return delta_time
@@ -107,9 +109,8 @@ class ptt_and_norm_reader:
         return light_senor
     def ptt_and_norm(self):
         pt_drawing = self.ptt_and_norm_draw()
-        for i in pt_drawing:
-           
-        return pt_drawing
+        for i in pt_drawing:         
+          return pt_drawing
 
       
       
