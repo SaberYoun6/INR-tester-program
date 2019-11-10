2b2af0823af5ed68bd83f16b14df531e984249ce
yuriprym <samuel.young.103@gmail.com>
2019-11-10 00:29:12 -0500

    add in a whole lot of unessary files in which I am going to have to add to teh gitignore list

--- iNR.py
+++ iNR.py
@@ -7,11 +7,11 @@
 # 
 #
 __author__ = "Samuel Young"
-__copyright__ =" Copyright 2016, The INR project"
+__copyright__ = " Copyright 2016, The INR project"
 __credits__ = ["Samuel Young"]
 __license__ = "GPL"
 __version__ = "0.0.7"
-__maintianer__= "Samuel Young"
+__maintianer__ = "Samuel Young"
 __email__ = "samuel.young.103@gmail.com"
 __status__ = "beta"
 #### dependencies ##### 
@@ -19,67 +19,72 @@ __status__ = "beta"
 import time
 import RPi.GPIO as GPIO
 import collections
-#from datetime import timedelta
-#from threading
+
+# from datetime import timedelta
+# from threading
 
 
 #### variables ####
-#ptdrawc2= input('INR')
-#ptnomdrawc2= int(ptdrawc2)
+# ptdrawc2= input('INR')
+# ptnomdrawc2= int(ptdrawc2)
 ##### GPIO  #######
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
-
 class LightSensor(object):
     def __init__(self, light, reflection):
         self.light = light
-        self.reflection= reflection
-    
-    def reset():
-       GPIO.cleanup()
-       print("Gpio Cleanup has been done")
-    
-    
-    def light_Sensor(self):
-        delta_time = 0.0
-        value= True
+        self.reflection = reflection
+
+    @staticmethod
+    def reset(self):
+        GPIO.cleanup()
+        print("Gpio Cleanup has been done")
+
+    @property
+    def sensor(self):
+        change = 0.0
+        value = True
         print("setting up  pins")
         GPIO.setup(self.reflection, GPIO.OUT)
-        GPIO.output(self.reflection,GPIO.LOW)
-        GPIO.setup(self.light,GPIO.OUT) 
-        #led_off= GPIO.output(self.light,GPIO.LOW)
-        #time.sleep(7.5)
+        GPIO.output(self.reflection, GPIO.LOW)
+        GPIO.setup(self.light, GPIO.OUT)
+        # led_off= GPIO.output(self.light,GPIO.LOW)
+        # time.sleep(7.5)
         print("testing light")
-        led_on=GPIO.output(self.light,GPIO.HIGH)
+        on = GPIO.output(self.light, GPIO.HIGH)
         time.sleep(1)
         print("Setup the input pin")
-        GPIO.setup(self.reflection,GPIO.IN)
-        init_time= time.perf_counter()
+        GPIO.setup(self.reflection, GPIO.IN)
+        reflector = GPIO.input(self.reflection)
+        timer = time.perf_counter()
         '''
         This  that the time should be below 90.0 second or until the value read true  
         '''
-        while ( (time.perf_counter - init_time) < 90.0   or  value == True):
-            refc= GPIO.input(self.reflection)
-            led_on
+        while (time.perf_counter - timer) < 90.0 or True == value:
+            reflector
+            on
             time.sleep(.0000000001)
-            final_time = time.perf_counter()
-            if refc == 0:
-               led_on
-               time.sleep(.0000000001)
-               delta_time=final_time-init_time
-            elif .01<= refc and refc <= .99:
-               led_on
-               time.sleep(.0000000001)
-               delta_time=final_time -init_time
-            else: 
-               GPIO.output(self.light,GPIO.LOW)
-               delta_time = final_time - init_time
-               value = False
-        return delta_time
+            finish = time.perf_counter()
+            if reflector == 0:
+                on
+                time.sleep(.0000000001)
+                change = finish - timer
+            elif .01 <= reflector <= .99:
+                on
+                time.sleep(.0000000001)
+                change = finish - timer
+            else:
+                GPIO.output(self.light, GPIO.LOW)
+                change = finish - time
+                value = False
+        return change
+
+
 #### this is the class that will be defined as how the device will calculate the ptt\ptNorm  values
+'''
 class ptt_and_norm_reader(object):
     def __init__(self, light, reflection):
         self.light = light
@@ -102,36 +107,38 @@ class ptt_and_norm_reader(object):
     def norm_reader(self):
         norm_drawing = self.norm_draw()
         return norm_drawing
-
+'''
 
 '''
 these are the four threads that I am trying to run to get any average for the individual 
 '''
-       
-def main(): 
-   ''' 
+
+
+def main():
+    '''
    each sensor is being called
    '''
-   l_sensor0 = LightSensor(24,18)
-   l_sersor1 = LightSensor(23,17)
-  # l_sensor2 = LightSensor(22,16)
-  # l_sensor3 = LightSensor(25,12)
-   '''
+    l_sensor0 = LightSensor(24, 18)
+    l_sensor1 = LightSensor(23, 17)
+    # l_sensor2 = LightSensor(22,16)
+    # l_sensor3 = LightSensor(25,12)
+    '''
    each value of the sensor is being retuend then printed
    '''
-   val_sensor0 = l_sensor0.light_Sensor()
-   val_sensor1 = l_sensor1.light_Sensor()
-   #val_sensor2 = l_sensor2.light_Sensor()
-   #val_sensor3 = l_sensor3.light_Sensor()
-   '''
+    val_sensor0 = l_sensor0.sensor
+    val_sensor1 = l_sensor1.sensor
+    # val_sensor2 = l_sensor2.light_Sensor()
+    # val_sensor3 = l_sensor3.light_Sensor()
+    '''
    what each value is being sent to the manchine
    '''
-   print(val_sensor0)
-   print(val_sensor1)
-   #print(val_sensor2)
-   #print(val_sensor3)
-   
-   l_senor0.reset()
+    print(val_sensor0)
+    print(val_sensor1)
+    # print(val_sensor2)
+    # print(val_sensor3)
+
+    l_sensor0.reset()
+
 
 if __name__ == "___main___":
     main()
@@ -141,5 +148,3 @@ GPIO.add_event_callback(l_Sensor1 , t2)
 GPIO.add_event_callback(l_Sensor2 , t3)
 GPIO.add_event_callback(l_Sensor3 , t4)
 '''
-
-
